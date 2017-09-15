26465b97f9426b046afbfcda0321f7d6b28d3b6b
alana.rae.smith@gmail.com <alana.rae.smith@gmail.com>
2017-09-15 15:24:09 -0500

    5 tests passing

--- test/ship_test.js
+++ test/ship_test.js
@@ -44,7 +44,7 @@ describe('checkForShip', function () {
         expect(checkForShip(player, [0, 0])).to.be.true;
     });
     
-    it('should handle ships located at more than one coordinate', function (){
+    it('should handle ships located at more than one coordinate', function () {
         
         player = {
             ships: [
@@ -56,5 +56,25 @@ describe('checkForShip', function () {
 
         expect(checkForShip(player, [0, 1])).to.be.true; 
         expect(checkForShip(player, [0, 0])).to.be.true;
+        expect(checkForShip(player, [9, 9])).to.be.false;
+    });
+
+    it('should handle checking multiple ships', function () {
+        
+        player = {
+            ships: [
+                {
+                    locations: [[0, 0], [0, 1]]
+                },
+                {
+                    locations: [[1, 0], [1, 1]]
+                }
+            ]
+        };
+
+        expect(checkForShip(player, [0, 1])).to.be.true; 
+        expect(checkForShip(player, [0, 0])).to.be.true;
+        expect(checkForShip(player, [1, 0])).to.be.true; 
+        expect(checkForShip(player, [1, 1])).to.be.true;
         expect(checkForShip(player, [9, 9])).to.be.false;
     });
